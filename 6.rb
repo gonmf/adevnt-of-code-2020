fa9a0eae84f516480f8c91919bc03c41d7650fd5
gonmf <gonmf@sapo.pt>
2020-12-06 09:59:48 +0000

    day 6

---
+++ 6.rb
@@ -0,0 +1,51 @@
+# problem 1
+
+input = File.read('6.input')
+
+acc = ''
+sum = 0
+
+input.split("\n").each do |line|
+  if line == ''
+    if acc != ''
+      sum += acc.chars.uniq.count
+      acc = ''
+    end
+  else
+    acc += line.strip
+  end
+end
+
+if acc != ''
+  sum += acc.chars.uniq.count
+end
+
+puts sum
+
+# problem 2
+
+def all_present(text)
+  texts = text.split('|').map(&:chars)
+
+  texts.reduce(texts[0]) { |acc, text| acc & text }.count
+end
+
+acc = nil
+sum = 0
+
+input.split("\n").each do |line|
+  if line == ''
+    if acc
+      sum += all_present(acc)
+      acc = nil
+    end
+  else
+    acc = acc ? "#{acc}|#{line.strip}" : line.strip
+  end
+end
+
+if acc
+  sum += all_present(acc)
+end
+
+puts sum
